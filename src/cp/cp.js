84bdb85863a1881ce587c56055161350078a7aa5
Vladimir <v.milashevsky@gmail.com>
2024-01-28 22:59:32 +0300

    feat: add solutions for Child_process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    console.log(args);
+
+    const childProcess = fork('./src/cp/files/script.js', args, {stdio: ['pipe', 'pipe', 'pipe','ipc']});
+
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.pipe(process.stdout);
+
+    childProcess.send('process.stdin');
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
